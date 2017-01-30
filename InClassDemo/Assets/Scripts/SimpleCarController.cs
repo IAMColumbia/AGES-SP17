beefd7298e8db42d3b6e41ea57d5f0e18f09f67e
David Antognoli <David.Antognoli@iam.local>
2017-01-30 15:15:21 -0600

    in class demo progress

---
+++ InClassDemo/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCarController : MonoBehaviour {
+    [SerializeField]
+    private float maxSteeringAngle = 30;
+
+    [SerializeField]
+    private float maxMotorTorque = 400;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForSteering;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForDriving;
+
+    private float driveInput;
+    private float steeringInput;
+    private Rigidbody rigidBody;
+
+    // Use this for initialization
+    private void Awake ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        driveInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+
+	}
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = maxSteeringAngle * steeringInput;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driveInput;
+        }
+
+
+        // Brakes?
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+
+
+         
+    }
+}
