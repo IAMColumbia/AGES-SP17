36ecfac371c9e7f4a1cdae7640e2991e1e17775c
Russell-Allen1 <Austin.Allen@iam.local>
2017-01-30 15:12:32 -0600

    Tank Game
    Tank Game for the midterm project.

--- Tank_Demo/Assets/Scripts/SimpleCarController.cs
+++ Tank_Demo/Assets/Scripts/SimpleCarController.cs
@@ -3,15 +3,51 @@ using System.Collections;
 
 public class SimpleCarController : MonoBehaviour {
 
+    [SerializeField]
+    float maxSteeringAngle = 30;
+
+    [SerializeField]
+    float maxMotorTorque = 400;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForSteering;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForDriving;
+
+    float driveInput;
+    float steeringInput;
+
+    private Rigidbody rigidBody;
+
 	// Use this for initialization
-	void Start ()
+	void Awake ()
     {
-	
+        rigidBody = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-	    
-	}
+        driveInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle= maxSteeringAngle * steeringInput;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driveInput;
+        }
+
+        //Brakes?
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+    }
+
+    
 }
