15dc81b268b94b83edaffe8f1c8f80717edefa71
jackmalboeuf <Jack.Malboeuf@iam.local>
2017-01-30 15:15:00 -0600

    In class demo

---
+++ TankDemo/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCarController : MonoBehaviour
+{
+    [SerializeField]
+    float maxSteeringAngle = 30;
+    [SerializeField]
+    float maxMotorTorque = 400;
+    [SerializeField]
+    WheelCollider[] wheelsUsedForSteering;
+    [SerializeField]
+    WheelCollider[] wheelsUsedForDriving;
+
+    float driveInput;
+    float steeringInput;
+    Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+    {
+        driveInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+	}
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = maxSteeringAngle * steeringInput;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driveInput;
+        }
+
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+    }
+}
