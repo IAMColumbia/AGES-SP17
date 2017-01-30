dbeae63e0988f0aa209cee8110bc3a80c51b4c6f
andrewfeagley <andrew.feagley@loop.colum.edu>
2017-01-30 15:16:23 -0600

    InClassDemo Progress
    Stopped before adding brakes

---
+++ InClassDemo/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCarController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxSteeringAngle = 30;
+    [SerializeField]
+    private float maxMotorTorque = 400;
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForSteering;
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForDriving;
+
+    private float driverInput;
+    private float steeringInput;
+    private Rigidbody rigidBody;
+
+    // Use this for initialization
+    private void Awake ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        driverInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+
+	}
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = maxSteeringAngle * steeringInput;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driverInput;
+        }
+
+        // Brakes:
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+    }
+}
