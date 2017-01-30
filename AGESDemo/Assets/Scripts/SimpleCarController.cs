bca19a02e8316c277eecc736e7d6c6eaf10588c5
andrewrines <andrew.rines@loop.colum.edu>
2017-01-30 15:14:36 -0600

    AGES Week 2 Car Demo created

---
+++ AGESDemo/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCarController : MonoBehaviour
+{
+    [SerializeField] private float maxSteeringAngle = 30;
+    [SerializeField] private float maxMotorTorque = 400;
+    [SerializeField] private WheelCollider[] wheelsUsedForSteering;
+    [SerializeField] private WheelCollider[] wheelsUsedForDriving;
+
+    private float driveInput;
+    private float steeringInput;
+    private Rigidbody myRigidbody;
+
+
+    void Awake ()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        driveInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+	}
+
+    void FixedUpdate()
+    {
+
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = maxSteeringAngle * steeringInput;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driveInput;
+        }
+
+        float forwardVelocity = transform.InverseTransformDirection(myRigidbody.velocity).z;
+    }
+
+}
