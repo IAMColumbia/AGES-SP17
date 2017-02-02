3678873f793c3b72bf198c931ba0167a0dff1b49
Zach Daniel <zachary.ryan.daniel@gmail.com>
2017-02-01 18:06:29 -0600

    in class work
    made a sick car

---
+++ inclass_02_01_17/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SimpleCarController : MonoBehaviour {
+
+    [SerializeField]
+    private WheelCollider[] steeringWheels;
+
+    [SerializeField]
+    private WheelCollider[] driveWheels;
+
+    private WheelCollider[] allWheelColliders;
+
+    [SerializeField]
+    private float maxSteerAngle = 30;
+
+    [SerializeField]
+    private float motorTorque = 500;
+
+    [SerializeField]
+    private float brakeTorque = 1500;
+
+    private float steeringInput = 0;
+    private float accelerationInput = 0;
+    private float brakingInput = 0;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	private void Start () {
+        allWheelColliders = GetComponentsInChildren<WheelCollider>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        GetInput();
+	}
+
+    private void FixedUpdate()
+    {
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxSteerAngle * steeringInput;
+        }
+
+        foreach(WheelCollider wheel in driveWheels)
+        {
+            wheel.motorTorque = motorTorque * accelerationInput;
+        }
+
+        foreach(WheelCollider wheel in allWheelColliders)
+        {
+            wheel.brakeTorque = brakeTorque * brakingInput;
+        }
+    }
+
+    private void GetInput()
+    {
+        steeringInput = Input.GetAxis("Horizontal");
+        accelerationInput = Input.GetAxis("Vertical");
+
+        float forwardComponentOfVelocity = Vector3.Dot(transform.forward, rb.velocity);
+
+        brakingInput = 0;
+
+        if(accelerationInput >= 0)
+        {
+            if(forwardComponentOfVelocity < 0)
+            {
+                brakingInput = accelerationInput;
+                accelerationInput = 0;
+            }
+        }
+        else
+        {
+            if(forwardComponentOfVelocity >= 0)
+            {
+                brakingInput = -accelerationInput;
+                accelerationInput = 0;
+            }
+        }
+
+        //brakingInput = Input.GetAxis("Brake");
+    }
+}
