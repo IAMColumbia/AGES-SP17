1d516c5ebb63b2b8d89b02388ed466e348f33263
MatveyDoronichev <Matvei.Doronichev@iam.local>
2017-02-01 18:09:02 -0600

    Week2_car_demo

---
+++ AGES_W2_Car/Assets/Scriprts/Carcontroller.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Carcontroller : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSteerAngle = 30;
+
+    [SerializeField]
+    private float maxMotorTouq = 300;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForSteering;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForDriving;
+
+
+    [SerializeField]
+    private float brakeTorque = 50;
+
+    [SerializeField]
+    private WheelCollider[] Allwheels;
+
+    private float steeringinput;
+    private float driveinput;
+    private Rigidbody rigitBody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigitBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+	}
+
+    private void FixedUpdate()
+    {
+
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = steeringinput * maxSteerAngle;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = driveinput * maxMotorTouq;
+        }
+
+        float forwardVelocity = transform.InverseTransformDirection(rigitBody.velocity).z;
+        for (int i = 0; i < Allwheels.Length; i++)
+        {
+          //TODO impliment breaking
+          //TODO if forward velocety = input -> add mototorouq,
+        }
+    }
+
+    private void GetInput()
+    {
+         steeringinput = Input.GetAxis("Horizontal");
+        driveinput = Input.GetAxis("Vertical");
+    }
+}
