77930bc4509e5d855ace295923d23e50d5e87fc8
JamesR95 <James.Robertson@iam.local>
2017-02-01 18:07:13 -0600

    In-Class 1: Car Demo Progress
    An in class example of creating car physics/controls in Unity. Also
    Trimmed down the files in the Couch Game assignment

---
+++ ClassDemo1JR/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SimpleCarController : MonoBehaviour
+{
+
+    [SerializeField]
+    float maxSteerAngle = 30;
+
+    [SerializeField]
+    float maxMotorTorque = 500;
+
+    [SerializeField]
+    float brakeTorque = 50;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForSteering;
+
+    [SerializeField]
+    private WheelCollider[] wheelsUsedForDriving;
+
+    [SerializeField]
+    private WheelCollider[] allWheelColliders;
+    
+    float steeringInput;
+    float driveInput;
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+	}
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = driveInput * maxMotorTorque;
+        }
+
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = steeringInput * maxSteerAngle;
+        }
+
+        for (int i = 0; i < allWheelColliders.Length; i++)
+        {
+            //TODO implement breaking
+            //if forwardVelocity matches input, then add motorTorque
+            //if forwardVelocity is opposite of input, then breakTorque
+        }
+    }
+
+    void GetInput()
+    {
+        steeringInput = Input.GetAxis("Horizontal");
+        driveInput = Input.GetAxis("Vertical");
+    }
+}
