f9a3a66093b4e3859edc5068824e9f194a6b1117
Russell-Allen1 <Austin.Allen@iam.local>
2017-02-06 15:18:25 -0600

    Tank_Demo
    Further work on the Tank_Demo midterm project.

--- Tank_Demo/Assets/Scripts/SimpleCarController.cs
+++ Tank_Demo/Assets/Scripts/SimpleCarController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SimpleCarController : MonoBehaviour {
 
@@ -10,15 +11,39 @@ public class SimpleCarController : MonoBehaviour {
     float maxMotorTorque = 400;
 
     [SerializeField]
+    float maxSpeedInMPH = 10;
+
+    [SerializeField]
+    private AnimationCurve torqueCurveModifier = 
+        new AnimationCurve(new Keyframe(0, 1), new Keyframe(20, 0.8f), new Keyframe(100, 0.3f));
+
+    [SerializeField]
+    float brakeTorque = 400;
+
+    [SerializeField]
     private WheelCollider[] wheelsUsedForSteering;
 
     [SerializeField]
     private WheelCollider[] wheelsUsedForDriving;
 
+    [SerializeField]
+    private WheelCollider[] allWheelColliders;
+
+    [SerializeField]
+    private Transform[] allWheelModels;
+
     float driveInput;
     float steeringInput;
 
     private Rigidbody rigidBody;
+
+    float ForwardVelocity
+    {
+        get
+        {
+            return transform.InverseTransformDirection(rigidBody.velocity).z;
+        }
+    }
 
 	// Use this for initialization
 	void Awake ()
@@ -29,25 +54,89 @@ public class SimpleCarController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+    }
+
+
+    private void FixedUpdate()
+    {
+        UpdateSteering();
+
+        UpdateMotorTorque();
+
+        UpdateBrakeTorque();
+
+        UpdateWheelModels();
+    }
+
+    void UpdateWheelModels()
+    {
+        Vector3 positionToSet;
+        Quaternion rotationToSet;
+
+        for (int i = 0; i < allWheelModels.Length; i++)
+        {
+            allWheelColliders[i].GetWorldPose(out positionToSet, out rotationToSet);
+            allWheelModels[i].position = positionToSet;
+            allWheelModels[i].rotation = rotationToSet;
+        }
+    }
+
+    void GetInput()
+    {
         driveInput = Input.GetAxis("Vertical");
         steeringInput = Input.GetAxis("Horizontal");
     }
 
-    private void FixedUpdate()
+    void UpdateSteering()
     {
         for (int i = 0; i < wheelsUsedForSteering.Length; i++)
         {
-            wheelsUsedForSteering[i].steerAngle= maxSteeringAngle * steeringInput;
+            wheelsUsedForSteering[i].steerAngle = maxSteeringAngle * steeringInput;
         }
-
-        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
-        {
-            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driveInput;
-        }
-
-        //Brakes?
-        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
     }
 
-    
+    void UpdateMotorTorque()
+    {
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = maxMotorTorque * driveInput 
+                * torqueCurveModifier.Evaluate(rigidBody.velocity.magnitude);
+        }
+        //Debug.Log("Current torqueCurveMod: " + torqueCurveModifier.Evaluate(rigidBody.velocity.magnitude));
+
+        CapSpeed();
+    }
+
+    private void CapSpeed()
+    {
+        const float milesPerHourConst = 2.23694f;
+
+        float speedInMPH = rigidBody.velocity.magnitude * milesPerHourConst;
+
+        if(speedInMPH > maxSpeedInMPH)
+        {
+            rigidBody.velocity = (maxSpeedInMPH / milesPerHourConst) * rigidBody.velocity.normalized;
+        }
+        Debug.Log("Speed in MPH " + speedInMPH);
+    }
+
+    void UpdateBrakeTorque()
+    {
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+
+        bool carIsMovingInSameDirectionAsInput = (forwardVelocity > 0) == (driveInput > 0);
+
+        float brakeTorqueToApply = 0f;
+
+        if (!carIsMovingInSameDirectionAsInput && driveInput != 0)
+        {
+            brakeTorqueToApply = brakeTorque;
+        }
+        for (int i = 0; i < allWheelColliders.Length; i++)
+        {
+            allWheelColliders[i].brakeTorque = brakeTorqueToApply;
+        }
+    }
+
 }
