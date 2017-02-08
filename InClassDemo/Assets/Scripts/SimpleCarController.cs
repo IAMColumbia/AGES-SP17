bfab9900988d583f4fec8531c8f61a1da5ee256c
danewheaton <Dane.Wheaton@iam.local>
2017-02-08 16:18:08 -0600

    in-class demo project of a car
    since I started using github in january, I use sourcetree more than the
    github desktop app, so I should become more familiar with it

---
+++ InClassDemo/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCarController : MonoBehaviour
+{
+    [SerializeField]
+    float maxSteerAngle = 30;
+
+    [SerializeField]
+    float maxMotorTorque = 500;
+
+    [SerializeField]
+    float brakeTorque = 50;
+
+    [SerializeField]
+    WheelCollider[] wheelsUsedForSteering;
+
+    [SerializeField]
+    WheelCollider[] wheelsUsedForDriving;
+
+    [SerializeField]
+    WheelCollider[] allWheelColliders;
+
+    Rigidbody rigidBody;
+
+    float steeringInput;
+    float driveInput;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+
+        for (int i = 0; i < wheelsUsedForSteering.Length; i++)
+        {
+            wheelsUsedForSteering[i].steerAngle = steeringInput * maxSteerAngle;
+        }
+
+        for (int i = 0; i < wheelsUsedForDriving.Length; i++)
+        {
+            wheelsUsedForDriving[i].motorTorque = driveInput * maxMotorTorque;
+        }
+
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+        for (int i = 0; i < allWheelColliders.Length; i++)
+        {
+            // TODO implement braking
+            // if forward velocity matches input, then add motor torque
+            // if forward velocity is opposite of input, add brake torque
+        }
+    }
+
+    void GetInput()
+    {
+        steeringInput = Input.GetAxis("Horizontal");
+        driveInput = Input.GetAxis("Vertical");
+    }
+}
