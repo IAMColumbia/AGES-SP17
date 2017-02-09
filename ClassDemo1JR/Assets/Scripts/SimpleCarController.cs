816dcdb634c1af1555b2c461045eb15442145663
JamesR95 <James.Robertson@iam.local>
2017-02-08 18:22:12 -0600

    Class 2 update
    Updated class car demo and fixed the gitignore file.

--- ClassDemo1JR/Assets/Scripts/SimpleCarController.cs
+++ ClassDemo1JR/Assets/Scripts/SimpleCarController.cs
@@ -22,10 +22,28 @@ public class SimpleCarController : MonoBehaviour
 
     [SerializeField]
     private WheelCollider[] allWheelColliders;
-    
+
+    [SerializeField]
+    private Transform[] allWheelModels;
+
+    [SerializeField]
+    private AnimationCurve torqueCurveModifier = new AnimationCurve(new Keyframe(0, 1), new Keyframe(100, 0.25f));
+
+    [SerializeField]
+    float maxSpeed = 50;
+
     float steeringInput;
     float driveInput;
     Rigidbody rigidBody;
+
+    private float ForwardVelocity
+    {
+        get
+        {
+            return transform.InverseTransformDirection(rigidBody.velocity).z;
+        }
+    }
+
 
     // Use this for initialization
     void Start ()
@@ -41,23 +59,73 @@ public class SimpleCarController : MonoBehaviour
 
     void FixedUpdate()
     {
+        UpdateSteering();
+
+        UpdateMotorTorque();
+
+        UpdateWheelModels();
+        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+
+        
+
+        float breakTorqueToApply = 0;
+
+        bool forwardVelocityIsSameDirectionAsInput = (ForwardVelocity > 0) == (driveInput > 0);
+
+        if (!forwardVelocityIsSameDirectionAsInput && driveInput != 0)
+            breakTorqueToApply = brakeTorque;
+
+        for (int i = 0; i < allWheelColliders.Length; i++)
+        {
+            allWheelColliders[i].brakeTorque = breakTorqueToApply;
+            //TODO implement breaking
+            //if forwardVelocity matches input, then add motorTorque
+            //if forwardVelocity is opposite of input, then breakTorque
+
+        }
+    }
+
+    private void UpdateWheelModels()
+    {
+        for (int i = 0; i < allWheelModels.Length; i++)
+        {
+            Vector3 positionToSet;
+            Quaternion rotationToSet;
+
+            allWheelColliders[i].GetWorldPose(out positionToSet, out rotationToSet);
+            allWheelModels[i].position = positionToSet;
+            allWheelModels[i].rotation = rotationToSet;
+        }
+    }
+
+    private void UpdateSteering()
+    {
+        float curveMod = torqueCurveModifier.Evaluate(rigidBody.velocity.magnitude);
         for (int i = 0; i < wheelsUsedForDriving.Length; i++)
         {
-            wheelsUsedForDriving[i].motorTorque = driveInput * maxMotorTorque;
+            wheelsUsedForDriving[i].motorTorque = driveInput * maxMotorTorque * curveMod;
         }
+    }
 
-        float forwardVelocity = transform.InverseTransformDirection(rigidBody.velocity).z;
+    private void UpdateMotorTorque()
+    {
         for (int i = 0; i < wheelsUsedForSteering.Length; i++)
         {
             wheelsUsedForSteering[i].steerAngle = steeringInput * maxSteerAngle;
         }
+    }
 
-        for (int i = 0; i < allWheelColliders.Length; i++)
+    private void CapSpeed()
+    {
+        const float mPHConversion = 2.23693629f;
+
+        float currentSpeedInMPH = rigidBody.velocity.magnitude * mPHConversion;
+        if (currentSpeedInMPH > maxSpeed)
         {
-            //TODO implement breaking
-            //if forwardVelocity matches input, then add motorTorque
-            //if forwardVelocity is opposite of input, then breakTorque
+            rigidBody.velocity = (currentSpeedInMPH / mPHConversion) * rigidBody.velocity.normalized;
+
         }
+        Debug.Log("Current Speed: " + currentSpeedInMPH);
     }
 
     void GetInput()
