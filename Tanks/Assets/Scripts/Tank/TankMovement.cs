9705672430bd00bb8b9c17b3e49108300b7d077c
Ryan Nolan <ryan.nolan70@gmail.com>
2017-02-12 06:19:19 -0600

    Tanks Tutorial Part 2
    Removed old Tanks tut, added new one

--- Tanks/Assets/Scripts/Tank/TankMovement.cs
+++ Tanks/Assets/Scripts/Tank/TankMovement.cs
@@ -92,6 +92,7 @@ public class TankMovement : MonoBehaviour
 
     private void Move()
     {
+        
         // Adjust the position of the tank based on the player's input.
         Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
 
