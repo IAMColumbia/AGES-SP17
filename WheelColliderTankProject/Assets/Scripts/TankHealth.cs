330bba684f588a3f07b709933820bc37c15e9dc1
NerovichM <Matthew.Nerovich@iam.local>
2017-02-13 14:35:59 -0600

    In Class Changes
    Changes Made In Class 2-13-17

---
+++ WheelColliderTankProject/Assets/Scripts/TankHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankHealth : MonoBehaviour, IDamagable {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TakeDamage()
+    {
+
+
+    }
+
+    public void checkDamage()
+    {
+
+    }
+}
