ea2ce8fc90a0ed6fe5bf20a450262d5c1e253900
GameDevSmitty <stgsmitty@gmail.com>
2017-02-13 15:17:48 -0600

    Inclass Realistic Tank Practice

---
+++ WheelColliderTankProject/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface IDamageable
+{
+    //Interface for damageable objects
+    void Damage();
+}
---
+++ WheelColliderTankProject/Assets/Scripts/TankHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TankHealth : MonoBehaviour, IDamageable
+{
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Damage()
+    {
+        throw new NotImplementedException();
+    }
+}
