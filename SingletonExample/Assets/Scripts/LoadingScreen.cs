e4f7b4c4e6f9ae880664e6e5fecdf2a980808c7f
GameDevSmitty <stgsmitty@gmail.com>
2017-02-20 15:24:32 -0600

    inclass stuff

---
+++ SingletonExample/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField]
+    string sceneToLoad;
+
+    [SerializeField]
+    GameObject loadingScreenPanel;
+
+    [SerializeField]
+    Text loadingText;
+
+    bool isSceenLoaded;
+
+    void Awake()
+    {
+        isSceenLoaded = false;
+        loadingScreenPanel.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+    }
+        
+    public void ButtonClicked()
+    {
+        Debug.Log("Button CLicked");
+        loadingScreenPanel.SetActive(true);
+        StartCoroutine(LoadNewScene());
+        StartCoroutine(LoadingTextMotion());
+    }
+
+    IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(2f);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        isSceenLoaded = true;
+        loadingScreenPanel.SetActive(false);
+    }
+
+    IEnumerator LoadingTextMotion()
+    {
+        float timeElapsed = 0;
+        
+        while(!isSceenLoaded)
+        {
+            timeElapsed += Time.deltaTime * 100;
+
+            if (timeElapsed % 20 > 0 && timeElapsed % 20 < 5)
+            {
+                loadingText.text = "Loading";
+            }
+            else if (timeElapsed % 20 > 5 && timeElapsed % 20 < 10)
+            {
+                loadingText.text = "Loading.";
+            }
+            else if (timeElapsed % 20 > 10 && timeElapsed % 20 < 15)
+            {
+                loadingText.text = "Loading..";
+            }
+            else if (timeElapsed % 20 > 15 && timeElapsed % 20 < 20)
+            {
+                loadingText.text = "Loading...";
+            }
+
+            Debug.Log(timeElapsed % 20);
+            yield return null;
+        }
+    }
+}
--- SingletonExample/Assets/Scripts/StaticCoin.cs
+++ SingletonExample/Assets/Scripts/StaticCoin.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StaticCoin : MonoBehaviour {
+public class StaticCoin : MonoBehaviour
+{
     public static int CoinCount { get; set; }
     private void OnMouseDown()
     {
+        StartCoroutine(WaitBeforeIncrementingCoins());
+    }
+
+    private IEnumerator WaitBeforeIncrementingCoins()
+    {
+        yield return new WaitForSeconds(2f);
+
         CoinCount++;
         Destroy(this.gameObject);
     }
+
 }
