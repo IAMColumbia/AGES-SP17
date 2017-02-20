d6aaa7af42a3c2f67f79d9b23a22230188bacc34
Nathan-Bowdish <Nathan.Bowdish@iam.local>
2017-02-20 15:25:28 -0600

    Blah
    Added a loading screen to the Singleton Pattern

--- SingletonExample/Assets/Scripts/StaticCoin.cs
+++ SingletonExample/Assets/Scripts/StaticCoin.cs
@@ -5,6 +5,14 @@ public class StaticCoin : MonoBehaviour {
     public static int CoinCount { get; set; }
     private void OnMouseDown()
     {
+        StartCoroutine(WaitBeforeDestroyCoin());
+
+    }
+
+    private IEnumerator WaitBeforeDestroyCoin()
+    {
+        yield return new WaitForSeconds(1f);
+
         CoinCount++;
         Destroy(this.gameObject);
     }
