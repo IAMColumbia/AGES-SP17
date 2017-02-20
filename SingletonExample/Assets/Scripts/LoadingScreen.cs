3d8b17798cd0f4880a9250d8819135f400f1edeb
chris-s-harper <chris.harper@loop.colum.edu>
2017-02-20 15:20:25 -0600

    Loading screen
    Worked on a loading screen with slider

---
+++ SingletonExample/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField]
+    string sceneToLoad;
+
+    [SerializeField]
+    GameObject loadingScreenPanel;
+
+    [SerializeField]
+    Slider progressSlider;
+
+    void Awake()
+    {
+        loadingScreenPanel.SetActive(false);
+        DontDestroyOnLoad(loadingScreenPanel);
+    }
+
+    public void ButtonClicked()
+    {
+        Debug.Log("Button Clicked!!!");
+
+        loadingScreenPanel.SetActive(true);
+
+        StartCoroutine(LoadNewScene());
+
+    }
+
+    private IEnumerator LoadNewScene()
+    {
+        progressSlider.value = 0;
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!async.isDone)
+        {
+            progressSlider.value = async.progress;
+            yield return null;
+        }
+
+
+        progressSlider.value = 1;
+
+        yield return new WaitForSeconds(0.5f);
+
+        loadingScreenPanel.SetActive(false);
+    }
+}
--- SingletonExample/Assets/Scripts/StaticCoin.cs
+++ SingletonExample/Assets/Scripts/StaticCoin.cs
@@ -5,6 +5,12 @@ public class StaticCoin : MonoBehaviour {
     public static int CoinCount { get; set; }
     private void OnMouseDown()
     {
+        StartCoroutine(WaitBeforeIncrementingCoins());
+    }
+
+    private IEnumerator WaitBeforeIncrementingCoins()
+    {
+        yield return new WaitForSeconds(2f);
         CoinCount++;
         Destroy(this.gameObject);
     }
