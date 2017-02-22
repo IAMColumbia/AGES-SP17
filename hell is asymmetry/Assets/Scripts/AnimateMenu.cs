b03369e7c849b200c41f7c3c11098b567f75abf9
Zach Daniel <zachary.ryan.daniel@gmail.com>
2017-02-22 14:34:18 -0600

    started work on "hell is asymmetry" bullet hell game
    rect transforms are the single most confusing thing in unity, but i'll
    bend them to my will

---
+++ hell is asymmetry/Assets/Scripts/AnimateMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnimateMenu : MonoBehaviour
+{
+    [SerializeField]
+    Image m_titleText;
+
+    Canvas m_canvas;
+
+    [SerializeField]
+    Camera m_cameraA, m_cameraB;
+
+    float m_halfWidth;
+    float m_fullHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_canvas = FindObjectOfType<Canvas>();
+
+        m_halfWidth = m_cameraA.aspect * m_cameraA.orthographicSize * 2;
+        m_fullHeight = m_cameraA.orthographicSize * 2;
+
+        Debug.Log(m_halfWidth);
+        Debug.Log(m_fullHeight);
+
+        RectTransform m_canvasRect = m_canvas.GetComponent<RectTransform>();
+
+        if(m_canvasRect == null)
+        {
+            throw new System.NullReferenceException("I don't know how you did it, but the canvas is missing its RectTransform");
+        }
+
+        m_canvasRect.sizeDelta = new Vector2(m_halfWidth * 2, m_fullHeight);
+        m_canvasRect.position = new Vector3(m_halfWidth/2, 0, 0);
+
+        m_titleText.rectTransform.offsetMin = new Vector2(m_halfWidth, m_titleText.rectTransform.offsetMin.y);
+        m_titleText.rectTransform.offsetMax = new Vector2(m_halfWidth, m_titleText.rectTransform.offsetMax.y);
+        m_titleText.rectTransform.sizeDelta = new Vector2(1,10000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
