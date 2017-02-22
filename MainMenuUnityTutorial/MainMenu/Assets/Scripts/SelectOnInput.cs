bbab49fed765ddab93752568542cdd3241baff70
Russell-Allen1 <Austin.Allen@iam.local>
2017-02-22 12:09:50 -0600

    MainMenuUnityTutorial
    Finished the Main Menu Unity Tutorial homework assignment.

---
+++ MainMenuUnityTutorial/MainMenu/Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem EventSystem;
+    public GameObject SelectedObject;
+
+    bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            EventSystem.SetSelectedGameObject(SelectedObject);
+            buttonSelected = true;
+        }
+	}
+
+    void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
