e4ac6241b698baf9242202dac8c4207abc5b8009
drdublchin17 <Szymon.Soltys@iam.local>
2017-02-22 18:09:32 -0600

    Class Work

---
+++ SingletonExample/Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour {
+    [SerializeField]
+    Slider progressSlider;
+
+    const string loadingSceneName = "LoadingScreen";
+    private static string sceneToLoad = "demo scene";
+
+	// Use this for initialization
+	void Start ()
+    {
+        progressSlider.value = 0;
+        StartCoroutine(BeginLoading());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void LoadNewScene(string sceneToLoad)
+    {
+
+        LoadingScene.sceneToLoad = sceneToLoad;
+        SceneManager.LoadScene(loadingSceneName);
+
+    }
+
+    private IEnumerator BeginLoading()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        AsyncOperation async =  SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+
+        while(!async.isDone)
+        {
+            progressSlider.value = async.progress;
+
+            if (async.progress > 0 && async.progress < 1)
+                yield return new WaitForSeconds(1f);
+
+            yield return null;
+        }
+
+        progressSlider.value = async.progress;
+
+        yield return new WaitForSeconds(0.5f);
+
+        SceneManager.UnloadScene(loadingSceneName);
+    }
+
+}
---
+++ SingletonExample/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    private string sceneToLoad;
+
+	public void StartGameButtonClicked()
+    {
+        LoadingScene.LoadNewScene(sceneToLoad);
+    }
+}
