146289cd16c4a3efc599d53c8e1c9e1a99c33a1f
Ryan Staudacher <Ryan.Staudacher@iam.local>
2017-02-22 18:07:13 -0600

    Singeton Example Update

---
+++ SingletonExample/Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField]
+    Slider progressSlider;
+
+    const string loadingSceneName = "LoadingScene";
+    private static string sceneToLoad = "demo scene";
+
+
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        progressSlider.value = 0;
+        StartCoroutine(BeginLoading());
+	}
+
+
+
+
+    
+    public static void LoadNewScene(string sceneToLoad)
+    {
+        LoadingScene.sceneToLoad = sceneToLoad;
+        SceneManager.LoadScene(loadingSceneName);
+    }
+
+
+
+
+    
+    private IEnumerator BeginLoading()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+
+        while(!async.isDone)
+        {
+            progressSlider.value = async.progress;
+
+            if(async.progress > 0 && async.progress < 1)
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            yield return null;
+        }
+
+        progressSlider.value = async.progress;
+
+        yield return new WaitForSeconds(0.5f);
+
+        SceneManager.UnloadScene(loadingSceneName);
+    }
+}
