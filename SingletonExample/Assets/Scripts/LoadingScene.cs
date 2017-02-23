090cad923dc910f2b88754254b6896c90370bcb0
amyai <amy.isaac@loop.colum.edu>
2017-02-22 18:11:44 -0600

    New Singleton Commit
    Hopefully not missing anything?

---
+++ SingletonExample/Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadingScene : MonoBehaviour
+
+{
+    [SerializeField]
+    Slider progressSlider;
+
+    private string sceneToLoad = "demo scene";
+   
+
+	// Use this for initialization
+	void Start ()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
