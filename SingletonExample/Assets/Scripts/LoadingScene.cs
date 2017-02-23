eb5bafb001cbb9a3769215c6489f70aa4615d67b
dboggsIAM <Dylan.Boggs@iam.local>
2017-02-22 18:02:51 -0600

    SingletonExampleUpdate

---
+++ SingletonExample/Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour {
+
+    [SerializeField]
+    Slider progressSlider;
+
+    const string loadingSceneName = "loading scene";
+    private static string sceneToLoad = "demo scene";
+	// Use this for initialization
+	void Start ()
+    {
+        progressSlider.value = 0;
+        StartCoroutine(BeginLoading());
+	}
+
+    public static void LoadNewScene(string sceneToLoad)
+    {
+        //SceneManager.LoadScene(sceneToLoad);
+        LoadingScene.sceneToLoad = sceneToLoad;
+        SceneManager.LoadScene(loadingSceneName);
+    }
+
+    private IEnumerator BeginLoading()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+       AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+
+        while (!async.isDone)
+        {
+            progressSlider.value = async.progress;
+            yield return null;
+        }
+
+        progressSlider.value = async.progress;
+
+        SceneManager.UnloadScene(loadingSceneName);
+    }
+}
---
+++ SingletonExample/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    string firstLevelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGameButtonClicked()
+    {
+        SceneManager.LoadScene("loading scene");
+    }
+}
