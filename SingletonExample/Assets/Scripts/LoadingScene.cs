199f9ddb03cf9f59447f6ad0676cf44a3749e2d8
thomas-mahon <thomas.mahon@loop.colum.edu>
2017-02-22 18:02:24 -0600

    LoadingScenePractice
    In class Singleton Pattern project added a main menu and loading screen
    with UI slider and async loading.

---
+++ SingletonExample/Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour {
+
+    [SerializeField]
+    Slider progressSlider;
+
+    private const string loadingSceneName = "LoadingScene";
+    private static string sceneToLoad = "demo scene";
+
+	// Use this for initialization
+	void Start () {
+        progressSlider.value = 0;
+        StartCoroutine(BeginLoading());
+	}
+	
+    public static void LoadNewScene(string sceneToLoad)
+    {
+        LoadingScene.sceneToLoad = sceneToLoad;
+        SceneManager.LoadScene(loadingSceneName);
+    }
+
+    
+    private IEnumerator BeginLoading()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+
+        while (!async.isDone)
+        {
+            progressSlider.value = async.progress;
+            yield return null;
+        }
+        
+        progressSlider.value = 1;
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.UnloadScene(loadingSceneName);
+    }
+}
