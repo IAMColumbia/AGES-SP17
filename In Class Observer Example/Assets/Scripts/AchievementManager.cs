c5c75841f6768a40b729a69f6e7c49bbd86d11f4
David Antognoli <David.Antognoli@iam.local>
2017-02-27 15:03:54 -0600

    observer example

---
+++ In Class Observer Example/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class AchievementManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject achievementPopup;
+
+    const int requiredKills = 3;
+
+
+    private void Start()
+    {
+        achievementPopup.SetActive(false);
+
+
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEnemyDied += CheckForUnlockingAchievement;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyDied -= CheckForUnlockingAchievement;
+    }
+
+    private void CheckForUnlockingAchievement()
+    {
+        if (Enemy.NumberOfEnemiesThatHaveDied == requiredKills)
+        {
+            DisplayAchievement();
+        }
+    }
+
+    private void DisplayAchievement()
+    {
+        achievementPopup.SetActive(true);
+        StartCoroutine(DismissAchievement());
+    }
+
+    private IEnumerator DismissAchievement()
+    {
+        yield return new WaitForSeconds(seconds: 2);
+
+        achievementPopup.SetActive(false);
+    }
+
+}
