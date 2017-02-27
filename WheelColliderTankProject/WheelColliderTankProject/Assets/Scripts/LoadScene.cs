37b11941c8530f18a59e54e5fac16cfb56134c09
Nathan-Bowdish <Nathan.Bowdish@iam.local>
2017-02-27 12:56:14 -0600

    Used the Loading Screen from last class in the WheelColliderTankProject
    I can make my own if you want (instead of using what was in class)

---
+++ WheelColliderTankProject/WheelColliderTankProject/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] string LevelToLoad;
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
---
+++ WheelColliderTankProject/WheelColliderTankProject/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] string sceneToLoad;
+    [SerializeField] GameObject loadingScreenPanel;
+    [SerializeField] Slider progressSlider;
+
+    void Awake()
+    {
+        loadingScreenPanel.SetActive(false);
+        DontDestroyOnLoad(loadingScreenPanel);
+        DontDestroyOnLoad(this);
+    }
+
+    public void ButtonClicked()
+    {
+        Debug.Log("Da buttun wuz click");
+        loadingScreenPanel.SetActive(true);
+        StartCoroutine(LoadNewScene());
+    }
+
+    private IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(0.5f);
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+        progressSlider.value = 0;
+
+        while(!async.isDone)
+        {
+            progressSlider.value = async.progress;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        progressSlider.value = async.progress;
+
+        loadingScreenPanel.SetActive(false);
+    }
+}
