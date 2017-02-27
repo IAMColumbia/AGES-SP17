3e8d1e669a429f4a432c5700a12d459680ce4a96
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-02-27 12:42:08 -0600

    singleton changes downloaded

---
+++ SingletonExample/Assets/Scripts/LoadGameButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadGameButton : MonoBehaviour
+{
+    [SerializeField] string sceneToLoad;
+    [SerializeField] GameObject loadingScreen;
+
+    void Awake()
+    {
+        loadingScreen.SetActive(false);
+        DontDestroyOnLoad(loadingScreen);
+    }
+    public void ButtonClicked()
+    {
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadNewScene());
+    }
+	
+    private IEnumerator LoadNewScene()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2);
+        loadingScreen.SetActive(false);
+        //progress bar
+    }
+
+}
