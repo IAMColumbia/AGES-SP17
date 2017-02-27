56df310b6da10738ebab67df6ba47994e86a3007
NerovichM <Matthew.Nerovich@iam.local>
2017-02-27 15:01:25 -0600

    In Class Observer Example

---
+++ In Class Observer Example/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject achievementPopUp;
+
+    const int requiredKills = 3;
+
+    private void Start()
+    {
+        achievementPopUp.SetActive(false);
+
+        Enemy.OnEnemyDied += checkForUnlockeingAchievement;
+    }
+
+    private void checkForUnlockeingAchievement()
+    {
+        if (Enemy.NumberOfEnemiesThatHaveDied == requiredKills)
+        {
+            DisplayAchievement();
+        }
+    }
+    private void DisplayAchievement()
+    {
+        achievementPopUp.SetActive(true);
+        StartCoroutine(DismissAchievement());
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEnemyDied += checkForUnlockeingAchievement;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyDied -= checkForUnlockeingAchievement;
+    }
+
+    private IEnumerator DismissAchievement()
+    {
+        yield return new WaitForSeconds(seconds: 2);
+    }
+}
---
+++ In Class Observer Example/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public delegate void EnemyDieDelegate();
+
+public class Enemy : MonoBehaviour {
+
+    public static event Action OnEnemyDied;
+
+    public static int NumberOfEnemiesThatHaveDied { get; private set; }
+
+	private void OnMouseDown()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        NumberOfEnemiesThatHaveDied++;
+        Destroy(gameObject);
+        if (OnEnemyDied != null)
+        {
+            OnEnemyDied.Invoke();
+        }
+    }
+}
