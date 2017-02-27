7db0554f28aefb351db78b6c80c9dc6597b4d8d5
chris-s-harper <chris.harper@loop.colum.edu>
2017-02-27 14:50:39 -0600

    In Class Observer Example
    Used for things like acheivements

---
+++ InClassObserverExample/Assets/Scripts/Acheivements.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Acheivements : MonoBehaviour
+{
+
+
+    [SerializeField]
+    GameObject acheivementPopup;
+
+    const int requiredkills = 3;
+
+
+    private void Start()
+    {
+        acheivementPopup.SetActive(false);
+
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEnemyDied += CheckForUnlockingAcheivement;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyDied -= CheckForUnlockingAcheivement;
+    }
+
+    private void CheckForUnlockingAcheivement()
+    {
+        if (Enemy.NumberOfEnemiesThatHaveDied == requiredkills)
+        {
+            DisplayAchievement();
+        }
+    }
+
+    private void DisplayAchievement()
+    {
+        acheivementPopup.SetActive(true);
+        StartCoroutine(DismissAcheivement());
+    }
+
+    private IEnumerator DismissAcheivement()
+    {
+        yield return new WaitForSeconds(seconds: 2);
+        acheivementPopup.SetActive(false);
+    }
+}
---
+++ InClassObserverExample/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//public delegate void EnemyDiedDelegate();
+
+public class Enemy : MonoBehaviour
+{
+    public static event Action OnEnemyDied;
+
+    public static int NumberOfEnemiesThatHaveDied { get; private set; }
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Player clicked " + gameObject.name);
+        Die();
+    }
+
+    private void Die()
+    {
+        NumberOfEnemiesThatHaveDied++;
+        Destroy(gameObject);
+
+        if (OnEnemyDied != null)
+        {
+            OnEnemyDied.Invoke();
+        }
+    }
+}
