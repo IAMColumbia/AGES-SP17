d83c95e26385fcdf20c7545df7602bfb60a6320c
David Antognoli <david.antognoli@gmail.com>
2017-03-01 12:54:40 -0600

    Observer example with some inheritance for achievements

---
+++ Observer Achievement Example 2/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class AchievementManager : MonoBehaviour 
+{
+    private List<Achievement> allAchievements;
+
+	// Use this for initialization
+	void Start ()
+    {
+        BuildAchievementList();
+
+        InitializeAchievements();
+    }
+
+    private void InitializeAchievements()
+    {
+        foreach (var achievement in allAchievements)
+        {
+            achievement.Initialize();
+        }
+    }
+
+    private void BuildAchievementList()
+    {
+        allAchievements = new List<Achievement>();
+
+        Achievement achievement = new KillEnemiesAchievement();
+        allAchievements.Add(achievement);
+    }
+
+    private class KillEnemiesAchievement : Achievement
+    {
+        private const int requiredKills = 3;
+
+        public override void Evaluate()
+        {
+            if (!IsUnlocked)
+            {
+                if (Enemy.NumberKilled == requiredKills)
+                {
+                    IsUnlocked = true;
+
+                    Debug.Log("Achievement Unlocked!");
+                    base.Evaluate();
+                }
+            }
+        }
+
+        public override void Initialize()
+        {
+            Enemy.EnemyDied += Evaluate;
+        }
+    }
+
+    private abstract class Achievement
+    {
+        public bool IsUnlocked { get; protected set; }
+        public abstract void Initialize();
+
+        public virtual void Evaluate()
+        {
+            if (IsUnlocked)
+            {
+                // TODO: display popup achievement panel!
+            }
+        }
+
+    }
+}
