cb6bbca7d683ed89427ac74c8b73053b5f002194
dboggsIAM <Dylan.Boggs@iam.local>
2017-03-01 17:55:44 -0600

    In-Class Observer Demo
    Includes the implementation of Achievements, using coroutines and the
    observer pattern.

---
+++ Section02ObserverDemo/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enemy : MonoBehaviour {
+
+    public static int numberOfEnemiesKilled;
+
+    public static event Action EnemyDied;
+
+    void OnMouseDown()
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        Debug.Log("The Enemy Died!");
+
+        numberOfEnemiesKilled++;
+
+        if (EnemyDied != null)
+        {
+            EnemyDied.Invoke();
+        }
+
+        Destroy(gameObject);
+    }
+}
