c081824ca6a6920fbc1e46713fe758b3bfcf302f
JamesR95 <James.Robertson@iam.local>
2017-03-01 18:06:12 -0600

    Observer Demo progress
    Missed some stuff but plan to check the wednesday section branch after I
    commit this

---
+++ ObersverDemo/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AchievementManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject achievementPopupPanel;
+
+    private List<Achievement> allAchievements;
+	// Use this for initialization
+	void Start ()
+    {
+        achievementPopupPanel.SetActive(false);
+        //StartCoroutine(TestAchievements());
+        allAchievements = new List<Achievement>();
+        Achievement achievement = new KillEnemiesAchievement();
+
+        Achievement.AchievementUnlocked += DisplayPopup;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        achievementPopupPanel.SetActive(true);
+	}
+
+    private void DisplayPopup()
+    {
+        achievementPopupPanel.SetActive(true);
+        StartCoroutine(DismissPopup());
+    }
+    private IEnumerator DismissPopup()
+    {
+        yield return new WaitForSeconds(seconds: 2);
+        achievementPopupPanel.SetActive(false);
+    }
+
+    private IEnumerator TestAchievements()
+    {
+        yield return new WaitForSeconds(seconds: 1);
+        DisplayPopup();
+
+        KillEnemiesAchievement achieve = new KillEnemiesAchievement();
+
+    }
+
+    private abstract class Achievement
+    {
+
+        private bool isUnlocked;
+
+        public static event Action AchievementUnlocked;
+
+        public bool IsUnlocked
+        {
+            get { return isUnlocked; }
+
+            protected set
+            {
+                isUnlocked = value;
+
+                if (isUnlocked)
+                {
+                    if (AchievementUnlocked != null)
+                        AchievementUnlocked.Invoke();
+                }
+            }
+        }
+
+        public abstract void Evaluate();
+
+        public virtual void Initialize()
+        {
+
+        }
+    }
+
+    private class KillEnemiesAchievement : Achievement
+    {
+        private const int requiredKills = 3;
+    
+        public override void Evaluate()
+        {
+            if (Enemy.NumberOfEnemiesThatHaveDied == requiredKills)
+            {
+                IsUnlocked = true;
+            }
+        }
+
+        public override void Initialize()
+        {
+            Enemy.EnemyDied += Evaluate;
+        }
+    }
+}
---
+++ ObersverDemo/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public static int NumberOfEnemiesThatHaveDied;
+
+    public static event Action EnemyDied;
+
+    void OnMouseDown()
+    {
+        Die();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        EnemyDied = Die;
+        EnemyDied += AnotherFunction;
+	}
+
+    void Die()
+    {
+        Debug.Log("The enemy died");
+        Destroy(gameObject);
+
+        if (EnemyDied != null)
+        {
+            EnemyDied.Invoke();
+        }
+
+        Destroy(gameObject);
+    }
+}
