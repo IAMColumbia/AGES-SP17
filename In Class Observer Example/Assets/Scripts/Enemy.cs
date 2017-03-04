b784dc9997158b3c937c2600e8863b904a30896f
andrew.feagley@loop.colum.edu <a1chemy>
2017-03-04 14:47:59 -0600

    in class observer
    github acting weird

---
+++ In Class Observer Example/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//public delegate void enemyDieDelegate();
+
+public class Enemy : MonoBehaviour
+{
+    public static event Action OnEnemyDie;
+
+    public static int NumberOfEnemiesThatHaveDied { get; private set; }
+
+    void OnMouseDown()
+    {
+        //Debug.Log("Player clicked " + gameObject.name);
+        Die();
+    }
+
+    private void Die()
+    {
+        NumberOfEnemiesThatHaveDied++;
+        Destroy(gameObject);
+
+        if(OnEnemyDie != null)
+            OnEnemyDie.Invoke();
+    }
+}    
