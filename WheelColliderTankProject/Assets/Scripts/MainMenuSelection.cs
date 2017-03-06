04945c78d52de9a4247dcd0612d5e74050710fdf
jackmalboeuf <Jack.Malboeuf@iam.local>
2017-03-06 15:17:14 -0600

    3/6

---
+++ WheelColliderTankProject/Assets/Scripts/MainMenuSelection.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuSelection : MonoBehaviour
+{
+    public int NumberOfPlayers { get; set; }
+    public GameObject Player1Weapon1 { get; set; }
+    public GameObject Player1Weapon2 { get; set; }
+    public GameObject Player2Weapon1 { get; set; }
+    public GameObject Player2Weapon2 { get; set; }
+    public GameObject Player3Weapon1 { get; set; }
+    public GameObject Player3Weapon2 { get; set; }
+    public GameObject Player4Weapon1 { get; set; }
+    public GameObject Player4Weapon2 { get; set; }
+}
