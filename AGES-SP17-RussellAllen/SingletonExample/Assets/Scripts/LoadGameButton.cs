b84052e11bb47189c5223a8e8e3289e5fe06ccfd
Russell-Allen1 <Austin.Allen@iam.local>
2017-03-07 17:06:25 -0600

    Midterm Project: Got blocks to spawn, need to fix on where they spawn.
    Got blocks to spawn, need to fix on where they spawn.

---
+++ AGES-SP17-RussellAllen/SingletonExample/Assets/Scripts/LoadGameButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadGameButton : MonoBehaviour
+{
+    [SerializeField]
+    string sceneToLoad;
+
+    [SerializeField]
+    GameObject loadingScreen;
+
+    void Awake()
+    {
+        loadingScreen.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ButtonClicked()
+    {
+        Debug.Log("button clicked");
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadNewScene());
+        Debug.Log("12");
+    }
+    
+    private IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(2f);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        loadingScreen.SetActive(false);
+    }	
+}
---
+++ AGES-SP17-RussellAllen/SingletonExample/Assets/Scripts/StaticCoin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticCoin : MonoBehaviour {
+    public static int CoinCount { get; set; }
+    private void OnMouseDown()
+    {
+        StartCoroutine(WaitBeforeIncrementingCoins());
+    }
+
+    private IEnumerator WaitBeforeIncrementingCoins()
+    {
+        yield return new WaitForSeconds(2f);
+
+        CoinCount++;
+        Destroy(this.gameObject);
+    }
+}
