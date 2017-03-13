d9adbd27fb895fd878b56efe8c78ecf04f5efb5b
CHRIS-PC\cshar <chris.harper@loop.colum.edu>
2017-03-12 19:50:53 -0500

    Added loading screen functionality

---
+++ Projcect1/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField]
+    GameObject loadingScreenPanel;
+    [SerializeField]
+    Slider progressSlider;
+
+    // Use this for initialization
+    void Awake()
+    {
+        loadingScreenPanel.SetActive(false);
+        DontDestroyOnLoad(loadingScreenPanel);
+    }
+
+    public void ButtonClicked(string sceneName)
+    {
+        loadingScreenPanel.SetActive(true);
+        StartCoroutine(LoadNewScene(sceneName));
+    }
+
+    private IEnumerator LoadNewScene(string sceneToLoad)
+    {
+        progressSlider.value = 0;
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!async.isDone)
+        {
+            progressSlider.value = async.progress;
+            yield return null;
+        }
+
+        progressSlider.value = 1;
+        yield return new WaitForSeconds(0.5f);
+        loadingScreenPanel.SetActive(false);
+    }
+}
