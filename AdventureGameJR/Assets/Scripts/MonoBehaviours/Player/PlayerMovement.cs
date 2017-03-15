956b4b8888494cbd9e9face46f210833c3bd869d
JamesR95 <james.robertson@loop.colum.edu>
2017-03-15 13:47:18 -0500

    Adventure Game 1a + 1b
    Here is what I got finished for both tutorials. I missed some stuff at
    the end of part B but I plan to finish that up by the end of today.

---
+++ AdventureGameJR/Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public Animator animator;
+    public NavMeshAgent agent;
+    public float inputHoldDelay = 0.5f;
+    public float turnSpeedThreshold = 0.5f;
+    public float speedDampTime = 0.1f;
+    public float slowingSpeed = 0.175f;
+    public float turnSmoothing = 15f;
+
+    private Vector3 destinationPosition;
+    private WaitForSeconds inputHoldWait;
+
+    private const float stopDistanceProportion = 0.1f;
+    private const float navMeshSampleDistance = 4f;
+
+    private readonly int hashSpeedPara = Animator.StringToHash("Speed");
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+
+        inputHoldWait = new WaitForSeconds(inputHoldDelay);
+
+        destinationPosition = transform.position;
+    }
+
+    private void OnAnimatorMove ()
+    {
+        agent.velocity = animator.deltaPosition / Time.deltaTime;
+    }
+
+    private void Update ()
+    {
+        if(agent.pathPending)
+        {
+            return;
+        }
+
+        float speed = agent.desiredVelocity.magnitude;
+
+        if (agent.remainingDistance <= agent.stoppingDistance * stopDistanceProportion)
+        {
+            Stopping(out speed);
+        }
+
+        else if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Slowing(out speed, agent.remainingDistance);
+        }
+
+        else if (speed > turnSpeedThreshold)
+        {
+            Moving();
+        }
+
+        animator.SetFloat(hashSpeedPara, speed, speedDampTime, Time.deltaTime);
+    }
+
+    private void Stopping(out float speed)
+    {
+        agent.Stop();
+        transform.position = destinationPosition;
+        speed = 0f;
+    }
+
+    private void Slowing(out float speed, float distanceToDestination)
+    {
+        agent.Stop();
+        transform.position = Vector3.MoveTowards(transform.position, destinationPosition, slowingSpeed * Time.deltaTime);
+        float proportionalDistance = 1f - distanceToDestination / agent.stoppingDistance;
+        speed = Mathf.Lerp(slowingSpeed, 0f, proportionalDistance);
+    }
+
+    private void Moving()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(agent.desiredVelocity);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+    }
+
+    public void OnGroundClick(BaseEventData data)
+    {
+        PointerEventData pData = (PointerEventData)data;
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(pData.pointerCurrentRaycast.worldPosition, out hit, navMeshSampleDistance, NavMesh.AllAreas))
+        {
+
+        }
+    }
+}
