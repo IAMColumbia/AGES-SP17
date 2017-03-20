0eb246ba256f05b3fd5c8d09dfc71935f088877f
CHRIS-PC\cshar <chris.harper@loop.colum.edu>
2017-03-20 01:40:52 -0500

    Attempted Camera Rig Setup (still needs to be fixed) and death border

---
+++ Projcect1/Assets/Scripts/DeathBorder1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathBorder1 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
