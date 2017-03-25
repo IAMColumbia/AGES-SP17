8946344cba2b8a5d1780e51232405f19f420b8ec
drdublchin17 <drdublchin17@gmail.com>
2017-03-25 00:07:00 -0700

    Mechanic Changes, now player picks up points

---
+++ Project1_AGES/Assets/Scripts/PickUp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter(Collider obj)
+	{
+		if(obj.tag == "Point")
+		{
+			Destroy (obj.gameObject);
+		}
+
+		if(obj.tag == "PowerUp")
+		{
+			Destroy (obj.gameObject);
+		}
+	}
+}
