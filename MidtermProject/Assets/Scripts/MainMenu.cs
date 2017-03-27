4a3579f1b228e1aa85842c686d107b3804d3fe2b
calebflick <calebflick@gmail.com>
2017-03-27 15:49:32 -0500

    Added Main Menu, Loading Screen, and 4 Player Gamepad Input

---
+++ MidtermProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    string firstLevelName;
+
+	public void StartGameButtonClicked()
+    {
+        LoadingScene.LoadNewScene(firstLevelName);
+    }
+
+    public void ExitGameButtonClicked()
+    {
+        Application.Quit();
+    }
+}
