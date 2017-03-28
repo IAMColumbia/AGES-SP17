9af7c0345101b02ce882ab3fafcbd345c2f11640
drdublchin17 <Szymon.Soltys@iam.local>
2017-03-27 22:01:16 -0500

    Implemented my own UI, further tweaked main menu, added scoring

---
+++ Project1_AGES/Assets/Scripts/KeepingCount.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KeepingCount : MonoBehaviour {
+
+    public GameObject AssignedPlayer;
+
+    private string CurrentScore;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CurrentScore = AssignedPlayer.GetComponent<PickUp>().PointsPickedUp.ToString();
+
+        gameObject.GetComponent<Text>().text = CurrentScore;
+	
+	}
+}
