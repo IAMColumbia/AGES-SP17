c64e16a82c74894bb84db7819e38bd4ed5270133
Russell-Allen1 <Austin.Allen@iam.local>
2017-03-29 13:12:09 -0500

    AdventureGameUnityTutorial: Finished the fourth (Conditions) video.
    Finished the fourth (Conditions) video.

--- AdventureGameUnityTutorial/Assets/Scripts/Editor/Interaction/Conditions/ConditionCollectionEditor.cs
+++ AdventureGameUnityTutorial/Assets/Scripts/Editor/Interaction/Conditions/ConditionCollectionEditor.cs
@@ -3,130 +3,97 @@ using UnityEditor;
 
 [CustomEditor(typeof(ConditionCollection))]
 public class ConditionCollectionEditor : EditorWithSubEditors<ConditionEditor, Condition>
-{
+{    
     public SerializedProperty collectionsProperty;
-
-
     private ConditionCollection conditionCollection;
     private SerializedProperty descriptionProperty;
     private SerializedProperty conditionsProperty;
     private SerializedProperty reactionCollectionProperty;
-
-
     private const float conditionButtonWidth = 30f;
     private const float collectionButtonWidth = 125f;
     private const string conditionCollectionPropDescriptionName = "description";
     private const string conditionCollectionPropRequiredConditionsName = "requiredConditions";
     private const string conditionCollectionPropReactionCollectionName = "reactionCollection";
-
-
-    private void OnEnable ()
+    private void OnEnable()
     {
         conditionCollection = (ConditionCollection)target;
-
         if (target == null)
         {
-            DestroyImmediate (this);
+            DestroyImmediate(this);
             return;
         }
-
         descriptionProperty = serializedObject.FindProperty(conditionCollectionPropDescriptionName);
         conditionsProperty = serializedObject.FindProperty(conditionCollectionPropRequiredConditionsName);
         reactionCollectionProperty = serializedObject.FindProperty(conditionCollectionPropReactionCollectionName);
-
-        CheckAndCreateSubEditors (conditionCollection.requiredConditions);
+        CheckAndCreateSubEditors(conditionCollection.RequiredConditions);
     }
-
-
-    private void OnDisable ()
+    private void OnDisable()
     {
-        CleanupEditors ();
+        CleanupEditors();
     }
-
-
-    protected override void SubEditorSetup (ConditionEditor editor)
+    protected override void SubEditorSetup(ConditionEditor editor)
     {
         editor.editorType = ConditionEditor.EditorType.ConditionCollection;
         editor.conditionsProperty = conditionsProperty;
     }
-
-
-    public override void OnInspectorGUI ()
+    public override void OnInspectorGUI()
     {
-        serializedObject.Update ();
+        serializedObject.Update();
+        CheckAndCreateSubEditors(conditionCollection.RequiredConditions);
 
-        CheckAndCreateSubEditors(conditionCollection.requiredConditions);
-        
         EditorGUILayout.BeginVertical(GUI.skin.box);
         EditorGUI.indentLevel++;
-
         EditorGUILayout.BeginHorizontal();
-
         descriptionProperty.isExpanded = EditorGUILayout.Foldout(descriptionProperty.isExpanded, descriptionProperty.stringValue);
-
         if (GUILayout.Button("Remove Collection", GUILayout.Width(collectionButtonWidth)))
         {
-            collectionsProperty.RemoveFromObjectArray (conditionCollection);
+            collectionsProperty.RemoveFromObjectArray(conditionCollection);
         }
-
         EditorGUILayout.EndHorizontal();
-        
+
         if (descriptionProperty.isExpanded)
         {
-            ExpandedGUI ();
+            ExpandedGUI();
         }
-        
+
         EditorGUI.indentLevel--;
         EditorGUILayout.EndVertical();
-
         serializedObject.ApplyModifiedProperties();
     }
-
-
-    private void ExpandedGUI ()
+    private void ExpandedGUI()
     {
         EditorGUILayout.Space();
-
         EditorGUILayout.PropertyField(descriptionProperty);
-
         EditorGUILayout.Space();
-
         float space = EditorGUIUtility.currentViewWidth / 3f;
-
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("Condition", GUILayout.Width(space));
         EditorGUILayout.LabelField("Satisfied?", GUILayout.Width(space));
         EditorGUILayout.LabelField("Add/Remove", GUILayout.Width(space));
         EditorGUILayout.EndHorizontal();
-
         EditorGUILayout.BeginVertical(GUI.skin.box);
         for (int i = 0; i < subEditors.Length; i++)
         {
             subEditors[i].OnInspectorGUI();
         }
         EditorGUILayout.EndHorizontal();
-
         EditorGUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace ();
+        GUILayout.FlexibleSpace();
         if (GUILayout.Button("+", GUILayout.Width(conditionButtonWidth)))
         {
             Condition newCondition = ConditionEditor.CreateCondition();
             conditionsProperty.AddToObjectArray(newCondition);
         }
         EditorGUILayout.EndHorizontal();
-
         EditorGUILayout.Space();
-
         EditorGUILayout.PropertyField(reactionCollectionProperty);
     }
-
-
     public static ConditionCollection CreateConditionCollection()
     {
         ConditionCollection newConditionCollection = CreateInstance<ConditionCollection>();
-        newConditionCollection.description = "New condition collection";
-        newConditionCollection.requiredConditions = new Condition[1];
-        newConditionCollection.requiredConditions[0] = ConditionEditor.CreateCondition();
+        newConditionCollection.Description = "New condition collection";
+        newConditionCollection.RequiredConditions = new Condition[1];
+        newConditionCollection.RequiredConditions[0] = ConditionEditor.CreateCondition();
         return newConditionCollection;
     }
 }
--- AdventureGameUnityTutorial/Assets/Scripts/ScriptableObjects/Interaction/Conditions/ConditionCollection.cs
+++ AdventureGameUnityTutorial/Assets/Scripts/ScriptableObjects/Interaction/Conditions/ConditionCollection.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 
-public class ConditionCollection : ScriptableObject { 
+public class ConditionCollection : ScriptableObject {
 
+    public string Description;
+    public Condition[] RequiredConditions = new Condition[0];
+    public ReactionCollection ReactionCollection;
 
     public bool CheckAndReact()
-    {        return true;
+    {
+        for (int i = 0; i < RequiredConditions.Length; i++)
+        {
+            if(!AllConditions.CheckCondition(RequiredConditions[i]))
+            {
+                return false;
+            }
+        }
+
+        if(ReactionCollection)
+        {
+            ReactionCollection.React();
+        }
+
+        return true;
     }
 }
