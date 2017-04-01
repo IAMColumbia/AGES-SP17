402cba799490cd9d0fb28b56866ffa5f386e3ae4
jackmalboeuf <jack.malboeuf@loop.colum.edu>
2017-04-01 15:00:15 -0500

    AdventureGameTutorial Parts 3 and 4

---
+++ AdventureGameTutorialPart2/Assets/Scripts/Editor/Inventory/InventoryEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof(Inventory))]
+public class InventoryEditor : Editor
+{
+    SerializedProperty itemImagesProperty;
+    SerializedProperty itemsProperty;
+    bool[] showItemSlots = new bool[Inventory.numItemSlots];
+
+    const string inventoryPropItemImagesName = "itemImages";
+    const string inventoryPropItemsName = "items";
+
+    void OnEnable()
+    {
+        itemImagesProperty = serializedObject.FindProperty(inventoryPropItemImagesName);
+        itemsProperty = serializedObject.FindProperty(inventoryPropItemsName);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        for (int i = 0; i < Inventory.numItemSlots; i++)
+        {
+            ItemSlotGUI(i);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void ItemSlotGUI(int index)
+    {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        EditorGUI.indentLevel++;
+
+        showItemSlots[index] = EditorGUILayout.Foldout(showItemSlots[index], "Item slot " + index);
+
+        if (showItemSlots[index])
+        {
+            EditorGUILayout.PropertyField(itemImagesProperty.GetArrayElementAtIndex(index));
+            EditorGUILayout.PropertyField(itemsProperty.GetArrayElementAtIndex(index));
+        }
+
+        EditorGUI.indentLevel--;
+        EditorGUILayout.EndVertical();
+    }
+}
