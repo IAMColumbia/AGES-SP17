cdbdb73cfc02b47a8c441755cb88aa4db825563f
RDC088 <ryancarpenter47@yahoo.com>
2017-04-01 00:06:04 -0500

    Adventure game tutorial part 2 - inventory completed, building UI, item management system, custom inspector

---
+++ Adventure Game part 2 - Inventory/Assets/Scripts/Editor/Inventory/InventoryEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor (typeof(Inventory))]
+public class InventoryEditor : Editor {
+
+    private SerializedProperty itemImagesProperty;
+    private SerializedProperty itemsProperty;
+    private bool[] showItemSlots = new bool[Inventory.numItemSlots];
+
+    private const string inventoryPropItemImagesName = "itemImages";
+    private const string inventoryPropItemsName = "items";
+
+    private void OnEnable()
+    {
+        itemImagesProperty = serializedObject.FindProperty(inventoryPropItemImagesName);
+        itemsProperty = serializedObject.FindProperty(inventoryPropItemsName);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        for (int i = 0; i < Inventory.numItemSlots; i++)
+        {
+            ItemSlotGUI(i);
+        }
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void ItemSlotGUI(int Index)
+    {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        EditorGUI.indentLevel++;
+
+        showItemSlots[Index] = EditorGUILayout.Foldout(showItemSlots[Index], "Item Slot " + Index);
+        if(showItemSlots[Index])
+        {
+            EditorGUILayout.PropertyField(itemImagesProperty.GetArrayElementAtIndex(Index));
+            EditorGUILayout.PropertyField(itemsProperty.GetArrayElementAtIndex(Index));
+        }
+
+        EditorGUI.indentLevel--;
+        EditorGUILayout.EndVertical();
+        
+    }
+}
