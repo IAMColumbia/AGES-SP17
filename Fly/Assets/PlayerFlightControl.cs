7afbad9639c15569e77419a3811ac6e710db01d0
3voker <DeAndre.Owens@iam.local>
2017-04-05 18:00:58 -0500

    Started Fly project

---
+++ Fly/Assets/PlayerFlightControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerFlightControl : MonoBehaviour {
+
+    // Use this for initialization
+    float moveHorizontal;
+    float moveVertical;
+	void Start () {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+	}
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 alwaysMoving;
+
+        alwaysMoving = Vector3.forward;
+        transform.position = alwaysMoving;
+        for (int i = 0; i < Time.fixedTime; i++)
+        {
+            while (i < Time.fixedTime)
+            {
+                transform.position = alwaysMoving;
+            }
+        }
+    }
+}
