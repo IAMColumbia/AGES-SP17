6e6550e5e0ac4b44d022388aa2b4630e8de354c9
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-04-06 21:57:53 -0500

    Needs controls displayed

---
+++ Ingargiola_Midterm/Ingargiola_TanksTutorial/Ingargiola_TanksTutorial/Assets/Scripts/UI/Button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] string sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+	
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
