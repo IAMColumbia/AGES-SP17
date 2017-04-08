b6b8ab54533d27eea7d629ab815c28fe0bd50f37
Nathan Bowdish <nathan.bowdish@loop.colum.edu>
2017-04-08 01:20:37 -0500

    Added Adventure Game Tutorials 4 and 5 (that is, videos 5 and 6)

---
+++ AllAdventureGameTutorials/AdventureGameTutorial5/Assets/Scripts/Editor/Interaction/InteractableEditor.cs
@@ -0,0 +1,70 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Interactable))]
+public class InteractableEditor : EditorWithSubEditors<ConditionCollectionEditor, ConditionCollection>
+{
+    private Interactable interactable;
+
+    private SerializedProperty interactionLocationProperty;
+    private SerializedProperty collectionsProperty;
+    private SerializedProperty defaultReactionCollectionProperty;
+
+    private const float collectionButtonWidth = 125f;
+    private const string interactablePropInteractionLocationName = "interactionLocation";
+    private const string interactablePropConditionCollectionsName = "conditionCollections";
+    private const string interactablePropDefaultReactionCollectionName = "defaultReactionCollection";
+
+    private void OnEnable()
+    {
+        collectionsProperty = serializedObject.FindProperty(interactablePropConditionCollectionsName);
+        interactionLocationProperty = serializedObject.FindProperty(interactablePropInteractionLocationName);
+        defaultReactionCollectionProperty = serializedObject.FindProperty(interactablePropDefaultReactionCollectionName);
+
+        interactable = (Interactable)target;
+
+        CheckAndCreateSubEditors(interactable.conditionCollections);
+    }
+
+    private void OnDisable()
+    {
+        CleanupEditors();
+    }
+
+    protected override void SubEditorSetup(ConditionCollectionEditor editor)
+    {
+        editor.collectionsProperty = collectionsProperty;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //whenever you're doing things in the editor, make sure you update at the start
+        serializedObject.Update();
+
+        CheckAndCreateSubEditors(interactable.conditionCollections);
+
+        EditorGUILayout.PropertyField(interactionLocationProperty);
+
+        for (int i = 0; i < subEditors.Length; i++)
+        {
+            subEditors[i].OnInspectorGUI();
+            EditorGUILayout.Space();
+        }
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if(GUILayout.Button("Add Collection", GUILayout.Width(collectionButtonWidth)))
+        {
+            ConditionCollection newCollection = ConditionCollectionEditor.CreateConditionCollection();
+            collectionsProperty.AddToObjectArray(newCollection);
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(defaultReactionCollectionProperty);
+
+        //and always apply this at the end!
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ AllAdventureGameTutorials/AdventureGameTutorial5/Assets/Scripts/MonoBehaviours/Interaction/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Transform interactionLocation;
+    public ConditionCollection[] conditionCollections = new ConditionCollection[0];
+    public ReactionCollection defaultReactionCollection;
+
+    public void Interact()
+    {
+        for (int i = 0; i < conditionCollections.Length; i++)
+        {
+            if(conditionCollections[i].CheckAndReact())
+            {
+                //if the condition was met, rect and stop checking
+                return;
+            }
+        }
+        defaultReactionCollection.React();
+    }
+}
