f133faec62fa1ee1483d7451d28c462d1d2f5b9f
redflamegard <Corey.King@iam.local>
2017-04-10 14:53:57 -0500

    FinalProject

---
+++ CoreyKingProject/HideandSeekV2/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    float moveHoriz;
+    float moveVert;
+
+    float mouseX;
+    [SerializeField] float moveSpeed;
+
+    Vector3 movement, rotation;
+
+    [SerializeField]
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        moveHoriz = Input.GetAxis("Horizontal");
+        moveVert = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        movement = new Vector3(moveHoriz, 0f, moveVert);
+        rotation = new Vector3(0f, mouseX, 0f);
+
+        transform.Translate(movement* moveSpeed * Time.deltaTime);
+        //rb.velocity = movement * moveSpeed;
+
+
+        this.transform.Rotate(rotation);
+	}
+}
