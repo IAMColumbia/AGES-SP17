1396bc2ac89cc95f394d9a88c524e834b252779b
RDC088 <ryancarpenter47@yahoo.com>
2017-04-10 00:30:12 -0500

    Final Project - warping mock up functional, camera functional

--- AGES final project/Assets/Scripts/SwitchWorlds.cs
+++ AGES final project/Assets/Scripts/SwitchWorlds.cs
@@ -5,19 +5,35 @@ public class SwitchWorlds : MonoBehaviour
 {
     [SerializeField]
     Transform world2Position;
+    [SerializeField]
+    Transform world1Position;
+
+    bool isInWorld2;
 	// Use this for initialization
 	void Start ()
     {
+        isInWorld2 = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if(!isInWorld2)
         {
-            this.transform.position = world2Position.position;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                this.transform.position = world2Position.position;
+                isInWorld2 = true;
+            }
         }
-	
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                this.transform.position = world1Position.position;
+                isInWorld2 = false;
+            }
+        }
 	}
 }
