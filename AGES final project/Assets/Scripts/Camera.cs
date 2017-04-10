3e3edc378a3bfe04ecc79622a729f12c3bda10c8
RDC088 <ryancarpenter47@yahoo.com>
2017-04-09 22:59:48 -0500

    Final Project - world switching mock up

---
+++ AGES final project/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToFollow;
+
+    Vector3 offset;
+	// Use this for initialization
+	void Start ()
+    {
+        offset = new Vector3();
+        offset.z = transform.position.z;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = objectToFollow.transform.position + offset;
+	
+	}
+}
---
+++ AGES final project/Assets/Scripts/SwitchWorlds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchWorlds : MonoBehaviour
+{
+    [SerializeField]
+    Transform world2Position;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            this.transform.position = world2Position.position;
+        }
+	
+	}
+}
