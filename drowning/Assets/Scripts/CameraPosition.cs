383f8a7d36849817abe7388c39f3de5a9a2eafcc
Zach Daniel <zachary.ryan.daniel@gmail.com>
2017-04-10 10:01:40 -0500

    Made a ton of sci fi stuff, camera moves to different locations with arrow keys

---
+++ drowning/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour {
+
+    //these are the animation trigger names
+    string[] cameraPositions =
+    {
+        "Center",
+        "Sonar",
+        "Engine"
+    };
+
+    int cameraPositionIndex = 0;
+
+    [SerializeField]
+    Animator m_animator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            cameraPositionIndex++;
+
+            if(cameraPositionIndex >= cameraPositions.Length) { cameraPositionIndex = 0; }
+
+            m_animator.SetTrigger(cameraPositions[cameraPositionIndex]);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            cameraPositionIndex--;
+
+            if (cameraPositionIndex < 0) { cameraPositionIndex = cameraPositions.Length - 1; }
+
+            m_animator.SetTrigger(cameraPositions[cameraPositionIndex]);
+        }
+	}
+}
---
+++ drowning/Assets/Scripts/PowerUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerUI : MonoBehaviour {
+
+    [SerializeField]
+    Text PowerText, PercentageText;
+
+    [SerializeField]
+    Image FillImage;
+
+    float t = 0;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t += Time.deltaTime * speed;
+        goToPercent(t);
+	}
+
+    void goToPercent(float percentage)
+    {
+        percentage = Mathf.Clamp01(percentage);
+        PercentageText.text = percentage.ToString("###.00%");
+        FillImage.fillAmount = percentage;
+    }
+}
