f3a8e087fb41cfa68f17e20d3bc58558511394e0
Ryan Staudacher <Ryan.Staudacher@iam.local>
2017-04-12 17:21:09 -0500

    Scene Changes to final project

---
+++ AGES Final Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Camera player1Camera;
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] Rigidbody2D rigidBody;
+    [SerializeField] string attachButton = "YellowAttach";
+
+    private Vector2 targetPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        MovePlayer();
+	}
+
+    void MovePlayer()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            targetPosition = player1Camera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Holding Point")
+        {
+            if(Input.GetButton(attachButton))
+            {
+                transform.position = other.transform.position;
+            }
+        }
+    }
+}
