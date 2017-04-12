999efd35b17f8743697269a9c19299f683c544cc
Ryan Staudacher <Ryan.Staudacher@iam.local>
2017-04-12 18:25:58 -0500

    Changes to Final Project

--- AGES Final Project/Assets/Scripts/PlayerMovement.cs
+++ AGES Final Project/Assets/Scripts/PlayerMovement.cs
@@ -31,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void OnCollisionStay2D(Collision2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if(other.gameObject.tag == "Holding Point")
         {
