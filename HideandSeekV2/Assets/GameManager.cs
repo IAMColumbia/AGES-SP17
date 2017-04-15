0f12b32ddcbca8294a6860d4582474ada08982d4
Corey King <corey.king@loop.colum.edu>
2017-04-14 19:29:46 -0500

    Worked on PlayerScripts
    is the Player a Hider or Seeker
    how is it determined if they are a hider or seeker

---
+++ HideandSeekV2/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    int numberOfSeeker, numberOfHiders;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+    void AssignPlayers (float player1, float player2, float player3, float player4)
+    {
+        
+
+
+    }
+}
---
+++ HideandSeekV2/Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    [SerializeField]
+    bool isSeeker, isHider, hasBeenTagged, isSpectating;
+
+
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSeeker == true)
+        {
+            this.gameObject.tag = "Seeker";
+
+        }
+
+        if (isHider == true)
+        {
+            this.gameObject.tag = "Hider";
+
+        }
+    }
+
+
+
+}
