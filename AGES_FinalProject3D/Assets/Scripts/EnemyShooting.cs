6427a51b9d5948ef536462b4367f8f0ceffdb750
chris-s-harper <chris.harper@loop.colum.edu>
2017-04-17 14:45:00 -0500

    Changed shooting AI to not inherit
    Self explanatory, implemented shooting AI better

---
+++ AGES_FinalProject3D/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour
+{
+    private GameObject playerToShootAt;
+
+    private SphereCollider activationZone;
+
+    private AudioSource laserAudio;
+
+    private bool isShooting;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerToShootAt = GameObject.Find("Player");
+        laserAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        LookAtAndShootPlayer();
+	}
+
+    private void LookAtAndShootPlayer()
+    {
+        gameObject.transform.LookAt(playerToShootAt.transform);
+    }
+
+    private void ShootingAudio()
+    {
+        if (isShooting && laserAudio.isPlaying == false)
+        {
+            laserAudio.Play();
+        }
+        if (!isShooting)
+        {
+            laserAudio.Stop();
+        }
+    }
+}
