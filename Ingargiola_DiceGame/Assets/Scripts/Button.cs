e86e5a09516cdd0934c40b63167d998e6e75d635
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-04-17 14:02:36 -0500

    Adds 30 tiles, Collider on top of cup, shake attmp

---
+++ Ingargiola_DiceGame/Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] GameObject Cup;
+    [SerializeField] float shakeSpeed = 1;
+
+
+    private void Update()
+    {
+        ShakeCup();
+    }
+    public void ShakeCup()
+    {
+        //transform.Translate
+    }
+}
---
+++ Ingargiola_DiceGame/Assets/Scripts/Cup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cup : MonoBehaviour
+{
+    //[SerializeField] 
+    Rigidbody cupRigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cupRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //RotateCup();
+	}
+
+    private void FixedUpdate()
+    {
+        cupRigidbody.AddRelativeForce(Vector3.forward * 10);
+    }
+
+
+
+    public void RotateCup()
+    {
+        transform.Rotate(0,0,120);
+    }
+}
