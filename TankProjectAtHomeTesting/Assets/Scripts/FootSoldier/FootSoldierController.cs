1555256578ca6f667ea13a67e9da5436cc519ea9
David Antognoli <david.antognoli@gmail.com>
2017-04-17 09:33:21 -0500

    Organizing scripts into subfolders, refactoring foot soldier code, made tank shell collider larger to hit small things on floor

---
+++ TankProjectAtHomeTesting/Assets/Scripts/FootSoldier/FootSoldierController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FootSoldierController : MonoBehaviour 
+{
+    #region Properties
+    public Player ControllingPlayer { get; private set; }
+    public bool IsInTank { get; private set; }
+    #endregion
+
+
+    #region Monobehaviour functions
+    private void Start()
+    {
+        IsInTank = false;
+        // TODO: this needs to be real and not hardcoded to P1
+        ControllingPlayer = new Player(1);
+    }
+    #endregion
+
+    private void EnterTank(TankController parentTank)
+    {        
+        IsInTank = true;
+        transform.SetParent(parentTank.transform);
+    }
+}
---
+++ TankProjectAtHomeTesting/Assets/Scripts/FootSoldier/FootSoldierMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootSoldierMovement : MonoBehaviour 
+{
+    #region Editor Fields
+    [SerializeField]
+    FootSoldierController footSoldierController;
+
+    [SerializeField]
+    Rigidbody movementRigidBody;
+
+    #endregion
+
+    #region Fields
+    private float xInput;
+    private float yInput;
+    private Vector3 moveDirection;
+    #endregion
+
+    #region Monobehaviour functions
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        ConvertInputToCameraRelative();
+        UpdateMovement();
+        UpdateRotation();
+    }
+    #endregion
+
+    private void GetInput()
+    {
+        xInput = Input.GetAxis("HorizontalP" + footSoldierController.ControllingPlayer.PlayerNumber);
+        yInput = Input.GetAxis("VerticalP" + footSoldierController.ControllingPlayer.PlayerNumber);
+    }
+
+    private void ConvertInputToCameraRelative()
+    {
+        moveDirection = new Vector3(yInput, 0, -xInput);
+        moveDirection = Camera.main.transform.InverseTransformDirection(moveDirection);
+    }
+
+    private void UpdateRotation()
+    {
+        // Debug.DrawRay(rigidbody.position, moveDirection * 3, Color.red);
+        float turnThreshold = 0.1f;
+        if (moveDirection.magnitude > turnThreshold)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            newRotation.eulerAngles = new Vector3(0, newRotation.eulerAngles.y, 0);
+            transform.rotation = newRotation;
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        float speed = 3;
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (moveDirection * speed * Time.deltaTime));
+    }
+}
