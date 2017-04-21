48759ce6dd4536b068e5adfc51c9e063fe5ae4c3
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-04-20 21:58:44 -0500

    updates cup coroutine, pawn movement

--- Ingargiola_DiceGame/Assets/Scripts/Cup.cs
+++ Ingargiola_DiceGame/Assets/Scripts/Cup.cs
@@ -5,9 +5,11 @@ public class Cup : MonoBehaviour
 {
     //[SerializeField] 
     Rigidbody cupRigidbody;
+    [SerializeField] GameObject cupAnchor;
+    [SerializeField] GameObject cupTopCollider;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         cupRigidbody = GetComponent<Rigidbody>();
 
@@ -44,18 +46,36 @@ public class Cup : MonoBehaviour
         yield return new WaitForSeconds(randomShakeTime);
         cupRigidbody.AddForce(Vector3.left * randomShakeSpeed);
 
-        yield return new WaitForSeconds(10);
-        //RotateCup();
+        yield return new WaitForSeconds(1);
+        cupRigidbody.velocity = Vector3.zero;
+        cupAnchor.SetActive(false);
+        RotateCup();
 
 
     }
 
-
+    //rotatecup as a coroutine that checks for what the Z-eulerangle is
+    //while its less than 120 keep rotating
     public void RotateCup()
     {
         //rotate on Z
         //transform.Rotate(0,0,120);
-
-        cupRigidbody.transform.Rotate(0,0,120);
+        //if z variable
+       
+        cupRigidbody.transform.Rotate(new Vector3(0,0,120), Time.deltaTime);
+        Debug.Log(cupRigidbody.gameObject.transform.eulerAngles.z);
+        cupTopCollider.SetActive(false);
+        
     }
 }
+
+
+
+/*
+ select infventory object and add homeobjectscript to the inventory object once it is in the house
+ if(gameObject.GetComponent<Pawn>() == null)   (could be in start to check if GameObject is set up correctly)
+        Pawn newPawn = gameObject.AddComponent<Pawn>();
+else
+        rest of script
+    
+*/
---
+++ Ingargiola_DiceGame/Assets/Scripts/Pawn.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pawn : MonoBehaviour
+{
+    [SerializeField] string movementAxisName;
+    [SerializeField] float pawnMovementX;
+    [SerializeField] float pawnMovementY;
+    [SerializeField] float pawnMovementZ;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PawnMovement();
+        //transform.Translate(Vector3.right * Time.deltaTime);
+
+    }
+
+    public void PawnMovement()
+    {
+        //TODO: move based on number rolled
+        //TODO: button to roll dice
+        //TODO: controls for minigames
+
+        if (Input.GetButtonDown(movementAxisName))
+            if (Input.GetAxisRaw(movementAxisName) > 0)
+                transform.Translate(pawnMovementX, pawnMovementY, pawnMovementZ);
+            else if(Input.GetAxisRaw(movementAxisName) < 0)
+                transform.Translate(pawnMovementX, pawnMovementY, -pawnMovementZ);
+
+
+        //trying to store current position of pawn
+        Vector3 currentPositino = transform.position;
+       print(currentPositino);
+       
+
+    }
+
+    public void PawnRollsDice()
+    {
+
+    }
+
+}
