1762c1778996ac9802679b21ba740fe5b7c3f89c
redflamegard <Corey.King@iam.local>
2017-04-27 13:42:50 -0500

    FixedLoadScreen

--- HideandSeekV2/Assets/Scripts/LoadScreen.cs
+++ HideandSeekV2/Assets/Scripts/LoadScreen.cs
@@ -32,11 +32,10 @@ public class LoadScreen : MonoBehaviour {
 
         }
 
-        // If the new scene has started loading...
+        
         if (loadScene == true)
         {
 
-            // ...then pulse the transparency of the loading text to let the player know that the computer is still working.
             loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
 
         }
@@ -44,18 +43,17 @@ public class LoadScreen : MonoBehaviour {
     }
 
 
-    // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
+
     IEnumerator LoadNewScene()
     {
 
-        // This line waits for 3 seconds before executing the next line in the coroutine.
-        // This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
+
         yield return new WaitForSeconds(15);
 
-        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+     
         AsyncOperation async = SceneManager.LoadSceneAsync(scene);
 
-        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+     
         while (!async.isDone)
         {
             yield return null;
