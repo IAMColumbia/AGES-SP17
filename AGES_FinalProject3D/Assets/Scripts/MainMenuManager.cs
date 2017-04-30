6fc8cbf65c04da19d9c39ad63f5dc9f6259e7085
CHRIS-PC\cshar <chris.harper@loop.colum.edu>
2017-04-30 16:28:26 -0500

    Implimented basic main menu

--- AGES_FinalProject3D/Assets/Scripts/MainMenuManager.cs
+++ AGES_FinalProject3D/Assets/Scripts/MainMenuManager.cs
@@ -1,17 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour {
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private string cancelButton;
+
+    private GameObject TitlePanel;
+    private GameObject CreditsPanel;
+    private GameObject HowToPlayPanel;
 
 	// Use this for initialization
 	void Start ()
     {
-	
+        TitlePanel = GameObject.Find("TitlePanel");
+        CreditsPanel = GameObject.Find("CreditsPanel");
+        HowToPlayPanel = GameObject.Find("HowToPlayPanel");
+
+        CloseInstuctionsAndCredits();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        if (Input.GetButtonDown(cancelButton))
+        {
+            CloseInstuctionsAndCredits();
+        }
 	}
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void OpenCreditsPanel()
+    {
+        TitlePanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        HowToPlayPanel.SetActive(false);
+    }
+
+    public void OpenHowToPlayPanel()
+    {
+        TitlePanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        HowToPlayPanel.SetActive(true);
+    }
+
+    public void CloseInstuctionsAndCredits()
+    {
+        TitlePanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        HowToPlayPanel.SetActive(false);
+    }
 }
