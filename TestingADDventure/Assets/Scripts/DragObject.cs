4737260b5364bb422a3a3fa699260a64e610ffec
jackmalboeuf <jack.malboeuf@loop.colum.edu>
2017-04-30 17:09:58 -0500

    4/30

---
+++ TestingADDventure/Assets/Scripts/DragObject.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentReturn;
+
+    public static GameObject draggedObject;
+    Vector3 startPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentReturn = transform.parent;
+        transform.SetParent(transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        //transform.parent.parent.parent.SetSiblingIndex(4);
+        transform.parent.SetAsLastSibling();
+
+        /*draggedObject = gameObject;
+        startPosition = transform.position;
+        parentReturn = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;*/
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentReturn);
+        transform.position = parentReturn.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        /*draggedObject = null;
+
+        if (transform.parent != parentReturn)
+        {
+            transform.position = startPosition;
+        }
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;*/
+    }
+}
+/*public void BeginDrag()
+	{
+        if (DraggingEnabled)
+        {
+            ParentReturn = transform.parent;
+		    transform.SetParent(transform.parent.parent);
+		    GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+            transform.parent.parent.parent.SetSiblingIndex(4);
+            transform.parent.SetAsLastSibling();
+        }
+    }
+
+	public void Drag()
+	{
+        if (DraggingEnabled)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+
+	public void EndDrag()
+	{
+        if (DraggingEnabled)
+        {
+            transform.SetParent(ParentReturn);
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+    }
+*/
---
+++ TestingADDventure/Assets/Scripts/DropSlot.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DropSlot : MonoBehaviour, IDropHandler
+{
+    /*public GameObject droppedObject
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }*/
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        DragObject d = eventData.pointerDrag.GetComponent<DragObject>();
+
+        d.parentReturn = transform;
+        d.transform.position = transform.position;
+        /*if (!droppedObject)
+        {
+            DragObject.draggedObject.transform.SetParent(transform);
+        }
+        else
+        {
+            Transform swap = DragObject.draggedObject.transform.parent;
+            DragObject.draggedObject.transform.SetParent(transform);
+            droppedObject.transform.SetParent(swap);
+        }*/
+    }
+}
+/*ObjectDrag d = eventData.pointerDrag.GetComponent<ObjectDrag>();
+
+        if (d.ItemType == SlotType && d.ItemSubType == SlotNumber)
+        {
+            d.ParentReturn = transform;
+        }
+*/
