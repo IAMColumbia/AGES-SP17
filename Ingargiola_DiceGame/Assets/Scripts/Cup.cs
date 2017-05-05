cfa0eeced26aae362ce08b1874ff3fc6938b3d21
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-05-05 02:52:36 -0500

    updates dice logic and resetting cup/dice
    had to get rid of current folder and reclone because all of sudden the
    cup stopped working properly. I came back after saving script progress
    and brought the project up to speed script wise. bummed i couldn't save
    the art and level design progress. oh well.

--- Ingargiola_DiceGame/Assets/Scripts/Cup.cs
+++ Ingargiola_DiceGame/Assets/Scripts/Cup.cs
@@ -3,40 +3,71 @@ using System.Collections;
 
 public class Cup : MonoBehaviour
 {
-    [SerializeField] GameObject cupAnchor;
-    [SerializeField] GameObject cupTopCollider;
+    [SerializeField]
+    GameObject cupAnchor;
+    [SerializeField]
+    GameObject cupTopCollider;
 
-    [SerializeField] float randomShakeTimeMin = 0f;
-    [SerializeField] float randomShakeTimeMax = 0.2f;
+    [SerializeField]
+    float randomShakeTimeMin = 0f;
+    [SerializeField]
+    float randomShakeTimeMax = 0.2f;
     //[SerializeField] float shakeTime = 0.2f;
 
-    [SerializeField] float randomShakeSpeedMin = 100f;
-    [SerializeField] float randomShakeSpeedMax = 300f;
+    [SerializeField]
+    float randomShakeSpeedMin = 100f;
+    [SerializeField]
+    float randomShakeSpeedMax = 300f;
 
-    [SerializeField] float delayBeforeRotating = 3;
-    [SerializeField] float delayBeforeResetting = 2;
+    [SerializeField]
+    float delayBeforeRotating = 3;
+    [SerializeField]
+    float delayBeforeResetting = 2;
 
+    [SerializeField]
+    string rollAxisName;
 
     Rigidbody cupRigidbody;
     Vector3 initialPosition;
     Quaternion initialRotation;
 
+    [SerializeField]
+    GameObject rollCamera;
+    [SerializeField]
+    GameObject diceGameObject;
+    Vector3 dicePositionInsideCup;
+    //Quaternion diceRotationInsideCup;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         initialPosition = transform.position;
         initialRotation = transform.rotation;
         cupRigidbody = GetComponent<Rigidbody>();
 
-       StartCoroutine(Shake());
+        //diceRotationInsideCup = diceGameObject.GetComponent<Transform>().rotation;
+        dicePositionInsideCup = diceGameObject.GetComponent<Transform>().position;
+        print("This is the dice's position inside the cup: "
+                    + dicePositionInsideCup);
 
-	}//end Start()
-	
-	// Update is called once per frame
-	void Update ()
+
+    }//end Start()
+
+    // Update is called once per frame
+    void Update()
     {
+        InitiateDiceRoll();
 
-	}//end Update()
+    }//end Update()
+
+    public void InitiateDiceRoll()
+    {
+        if (Input.GetButtonDown(rollAxisName))
+        {
+            rollCamera.SetActive(true);
+            StartCoroutine(Shake());
+        }
+    }
 
     public IEnumerator Shake()
     {
@@ -56,7 +87,7 @@ public class Cup : MonoBehaviour
         cupRigidbody.velocity = Vector3.zero;
         cupAnchor.GetComponent<ConfigurableJoint>().connectedBody = null;
         cupRigidbody.isKinematic = true;
-        
+
         cupTopCollider.SetActive(false);
         StartCoroutine(RotateCup());
 
@@ -64,9 +95,12 @@ public class Cup : MonoBehaviour
 
     void ResetCup()
     {
+        rollCamera.SetActive(false);
+
         transform.position = initialPosition;
         transform.rotation = initialRotation;
-        //ToDO: Put dice back in cup!
+        diceGameObject.transform.position = dicePositionInsideCup;
+
         cupTopCollider.SetActive(true);
         cupAnchor.GetComponent<ConfigurableJoint>().connectedBody = cupRigidbody;
         cupRigidbody.isKinematic = false;
@@ -77,9 +111,9 @@ public class Cup : MonoBehaviour
     {
         float rotationTime = .5f;
         float rotationSpeed = 5;
-        
 
-        for (float i = 0; i < rotationTime; i+=Time.deltaTime)
+
+        for (float i = 0; i < rotationTime; i += Time.deltaTime)
         {
             transform.Rotate(0, 0, rotationSpeed);
             yield return null;
@@ -87,8 +121,138 @@ public class Cup : MonoBehaviour
 
         yield return new WaitForSeconds(delayBeforeResetting);
         ResetCup();
-        
+
     }//end Coroutine RotateCup()
 
 }//end Cup Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using UnityEngine;
+//using System.Collections;
+
+//public class Cup : MonoBehaviour
+//{
+//    [SerializeField] GameObject cupAnchor;
+//    [SerializeField] GameObject cupTopCollider;
+
+//    [SerializeField] float randomShakeTimeMin = 0f;
+//    [SerializeField] float randomShakeTimeMax = 0.2f;
+//    //[SerializeField] float shakeTime = 0.2f;
+
+//    [SerializeField] float randomShakeSpeedMin = 100f;
+//    [SerializeField] float randomShakeSpeedMax = 300f;
+
+//    [SerializeField] float delayBeforeRotating = 3;
+//    [SerializeField] float delayBeforeResetting = 2;
+
+
+//    Rigidbody cupRigidbody;
+//    Vector3 initialPosition;
+//    Quaternion initialRotation;
+
+//    // Use this for initialization
+//    void Start ()
+//    {
+//        initialPosition = transform.position;
+//        initialRotation = transform.rotation;
+//        cupRigidbody = GetComponent<Rigidbody>();
+
+//       StartCoroutine(Shake());
+
+//	}//end Start()
+
+//	// Update is called once per frame
+//	void Update ()
+//    {
+
+//	}//end Update()
+
+//    public IEnumerator Shake()
+//    {
+//        float randomShakeSpeed = Random.Range(randomShakeSpeedMin, randomShakeSpeedMax);
+//        float randomShakeTime = Random.Range(randomShakeTimeMin, randomShakeTimeMax);
+
+//        cupRigidbody.AddForce(Vector3.forward * randomShakeSpeed);
+//        print(randomShakeSpeed);
+
+//        yield return new WaitForSeconds(randomShakeTime);
+//        cupRigidbody.AddForce(Vector3.back * randomShakeSpeed);
+
+//        yield return new WaitForSeconds(randomShakeTime);
+//        cupRigidbody.AddForce(Vector3.left * randomShakeSpeed);
+
+//        yield return new WaitForSeconds(delayBeforeRotating);
+//        cupRigidbody.velocity = Vector3.zero;
+//        cupAnchor.GetComponent<ConfigurableJoint>().connectedBody = null;
+//        cupRigidbody.isKinematic = true;
+
+//        cupTopCollider.SetActive(false);
+//        StartCoroutine(RotateCup());
+
+//    } //end Coroutine Shake()
+
+//    void ResetCup()
+//    {
+//        transform.position = initialPosition;
+//        transform.rotation = initialRotation;
+//        //ToDO: Put dice back in cup!
+//        cupTopCollider.SetActive(true);
+//        cupAnchor.GetComponent<ConfigurableJoint>().connectedBody = cupRigidbody;
+//        cupRigidbody.isKinematic = false;
+
+//    }//end ResetCup()
+
+//    public IEnumerator RotateCup()
+//    {
+//        float rotationTime = .5f;
+//        float rotationSpeed = 5;
+
+
+//        for (float i = 0; i < rotationTime; i+=Time.deltaTime)
+//        {
+//            transform.Rotate(0, 0, rotationSpeed);
+//            yield return null;
+//        }
+
+//        yield return new WaitForSeconds(delayBeforeResetting);
+//        ResetCup();
+
+//    }//end Coroutine RotateCup()
+
+//}//end Cup Class
 
--- Ingargiola_DiceGame/Assets/Scripts/DiceSideLogic.cs
+++ Ingargiola_DiceGame/Assets/Scripts/DiceSideLogic.cs
@@ -1,23 +1,177 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class DiceSideLogic : MonoBehaviour {
+public class DiceSideLogic : MonoBehaviour
+{
+    #region Dice Side Bools
+    bool diceSideOneIsDown = false;
+    bool diceSideTwoIsDown = false;
+    bool diceSideThreeIsDown = false;
+    bool diceSideFourIsDown = false;
+    bool diceSideFiveIsDown = false;
+    bool diceSideSixIsDown = false;
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    void OnTriggerStay(Collider col)
+    //[SerializeField] string diceSideOneName;
+    //[SerializeField] string diceSideTwoName;
+    //[SerializeField] string diceSideThreeName;
+    //[SerializeField] string diceSideFourName;
+    //[SerializeField] string diceSideFiveName;
+    //[SerializeField] string diceSideSixName;
+
+    // Use this for initialization
+    void Start()
     {
-        //TODO: adjust collider sizes so that only one is touching
-        if (col.tag == "Floor")
-            Debug.Log(this.name);
-        //switch if 
+        StartCoroutine(WaitUntilDiceStops());
+
     }
 
-}
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void ThisSideIsDown()
+    {
+        if (diceSideOneIsDown == true)
+            print("You rolled a 6");
+        if (diceSideTwoIsDown == true)
+            print("You rolled a 5");
+        if (diceSideThreeIsDown == true)
+            print("You rolled a 4");
+        if (diceSideFourIsDown == true)
+            print("You rolled a 3");
+        if (diceSideFiveIsDown == true)
+            print("You rolled a 2");
+        if (diceSideSixIsDown == true)
+            print("You rolled a 1");
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Dice")
+        {
+            print(col.gameObject.name);
+            switch (col.gameObject.name)
+            {
+                case "DiceSideCollision (1)": //Side 6 is up
+                    diceSideOneIsDown = true;
+                    //diceSideTwoIsDown = false;
+                    //diceSideThreeIsDown = false;
+                    //diceSideFourIsDown = false;
+                    //diceSideFiveIsDown = false;
+                    //diceSideSixIsDown = false;
+                    break;
+
+                case "DiceSideCollision (2)": //Side 5 is up
+                    diceSideTwoIsDown = true;
+                    //diceSideOneIsDown = false;
+                    //diceSideThreeIsDown = false;
+                    //diceSideFourIsDown = false;
+                    //diceSideFiveIsDown = false;
+                    //diceSideSixIsDown = false;
+                    break;
+
+                case "DiceSideCollision (3)": //Side 4 is up
+                    diceSideThreeIsDown = true;
+                    //diceSideOneIsDown = false;
+                    //diceSideTwoIsDown = false;
+                    //diceSideFourIsDown = false;
+                    //diceSideFiveIsDown = false;
+                    //diceSideSixIsDown = false;
+                    break;
+
+                case "DiceSideCollision (4)": //Side 3 is up
+                    diceSideFourIsDown = true;
+                    //diceSideOneIsDown = false;
+                    //diceSideTwoIsDown = false;
+                    //diceSideThreeIsDown = false;
+                    //diceSideFiveIsDown = false;
+                    //diceSideSixIsDown = false;
+                    break;
+
+                case "DiceSideCollision (5)": //Side 2 is up
+                    diceSideFiveIsDown = true;
+                    //diceSideOneIsDown = false;
+                    //diceSideTwoIsDown = false;
+                    //diceSideThreeIsDown = false;
+                    //diceSideFourIsDown = false;
+                    //diceSideSixIsDown = false;
+                    break;
+
+                case "DiceSideCollision (6)": //Side 1 is up
+                    diceSideSixIsDown = true;
+                    //diceSideOneIsDown = false;
+                    //diceSideTwoIsDown = false;
+                    //diceSideThreeIsDown = false;
+                    //diceSideFourIsDown = false;
+                    //diceSideFiveIsDown = false;
+                    break;
+
+                default:
+                    break;
+            }//end switch
+        }//end if
+         //Debug.Log(this.name);
+    }//end OnTriggerStay
+
+    private IEnumerator WaitUntilDiceStops()
+    {
+        yield return new WaitForSeconds(5);
+        ThisSideIsDown();
+    }
+}//end DiceSideLogic Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using UnityEngine;
+//using System.Collections;
+
+//public class DiceSideLogic : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+
+//	}
+
+//	// Update is called once per frame
+//	void Update () {
+
+//	}
+//    void OnTriggerStay(Collider col)
+//    {
+//        //TODO: adjust collider sizes so that only one is touching
+//        if (col.tag == "Floor")
+//            Debug.Log(this.name);
+//        //switch if 
+//    }
+
+//}
