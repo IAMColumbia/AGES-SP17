d68d1c460971976dcd15ea80024e0672107b8fc2
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-05-07 14:56:24 -0500

    Main Menu with button animations

---
+++ Ingargiola_DiceGame/Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SelectOnInput : MonoBehaviour
+{
+    [SerializeField] EventSystem eventSystem;
+    [SerializeField] GameObject selcetedGameObject;
+
+    private bool buttonSelected;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selcetedGameObject);
+            buttonSelected = true;
+        }
+	   
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
