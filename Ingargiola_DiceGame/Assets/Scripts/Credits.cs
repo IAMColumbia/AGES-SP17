86294b9619ba35ec084c5dc5573403557a361225
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-05-08 08:00:16 -0500

    Working Credits

---
+++ Ingargiola_DiceGame/Assets/Scripts/Credits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] string theURL;
+
+    public void LinkToResourceURL()
+    {
+        Application.OpenURL(theURL);
+    }
+	
+}
